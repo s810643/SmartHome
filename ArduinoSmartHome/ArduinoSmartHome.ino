b5a65bcf6b521330283a7702318fdfd8952e39b3
s810643 <myfsman@gmail.com>
2020-09-28 16:28:41 +0800

    修正復位後警示燈不會熄滅問題, close #1

--- ArduinoSmartHome/ArduinoSmartHome.ino
+++ ArduinoSmartHome/ArduinoSmartHome.ino
@@ -14,13 +14,17 @@ void loop() {
     Serial.println("LOW");
   }
   if(digitalRead(2)==HIGH) {
-  digitalWrite(12, HIGH);
-  digitalWrite(11, LOW);
-  tone(7, 1000, 500);
-  delay(500);
-  digitalWrite(12, LOW);
-  digitalWrite(11, HIGH);
-  tone(7, 800, 500);
-  delay(500);
+    digitalWrite(12, HIGH);
+    digitalWrite(11, LOW);
+    tone(7, 1000, 500);
+    delay(500);
+    digitalWrite(12, LOW);
+    digitalWrite(11, HIGH);
+    tone(7, 800, 500);
+    delay(500);
+  }
+  else {
+    digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
   }
 }
