9afefe600b3eae8ecb2c2693af1e285fa0daeb24
s810643 <myfsman@gmail.com>
2020-09-28 16:37:30 +0800

    依狀態不同送出 alert, normal 訊息給控制器

--- ArduinoSmartHome/ArduinoSmartHome.ino
+++ ArduinoSmartHome/ArduinoSmartHome.ino
@@ -14,6 +14,7 @@ void loop() {
     Serial.println("LOW");
   }
   if(digitalRead(2)==HIGH) {
+    Serial.println("alert");
     digitalWrite(12, HIGH);
     digitalWrite(11, LOW);
     tone(7, 1000, 500);
@@ -24,7 +25,9 @@ void loop() {
     delay(500);
   }
   else {
+    Serial.println("normal");
     digitalWrite(12, LOW);
     digitalWrite(11, LOW);
-  }
+    delay(1000);
+   }
 }
